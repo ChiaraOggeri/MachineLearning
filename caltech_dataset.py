2b77bf23c780d3f6fbf200b19da6c0a963bbce1f
ChiaraOggeri <chiara.oggeribreda@gmail.com>
2020-12-09 21:37:59 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -5,7 +5,7 @@ from PIL import Image
 import os
 import os.path
 import sys
-
+import pandas as pd
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -14,53 +14,9 @@ def pil_loader(path):
         return img.convert('RGB')
 
 
-
 class Caltech(VisionDataset):
-     def __init__(self, root, split='train', transform=None, target_transform=None):
+    def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
-
-        self.split = split # This defines the split you are going to use
-         
-        label=0
-        curlab=""
-        self.images=[]
-        self.labels=[]
-        if(self.split=='train'):
-        
-           f=open('MachineLearning/train.txt')
-           lines= f.readlines()
-           for line in lines:
-              parts=line.split('/')
-              if curlab=="":
-                   curlab=parts[0]
-              else:
-                    if curlab!=parts[0]:
-                        label=label+1
-                        curlab=parts[0]
-              line1=line.split('\n')
-              path1=root+"/"+line1[0]
-              self.images.append(pil_loader(path1))
-              self.labels.append(label)
-        else:
-           f=open("MachineLearning/test.txt")
-           lines= f.readlines()
-           for line in lines:
-               parts=line.split('/')
-               if curlab=="":
-                   curlab=parts[0]
-               else:
-                    if curlab!=parts[0]:
-                        label=label+1
-                        curlab=parts[0]
-               line1=line.split('\n')
-               path1=root+"/"+line1[0]
-               self.images.append(pil_loader(path1))
-               self.labels.append(label)
-               
-
-            
-     
-  
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -69,38 +25,37 @@ class Caltech(VisionDataset):
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
-     def __makesplit__(self):
-        self.trainsplit=[]
-        self.validsplit=[]
-        count=0
-        for i in range(0,len(self.images)):
-            if count<2:
-                self.trainsplit.append(i)
-                count+=1
-            else:
-                self.validsplit.append(i)
-                count=0
-    
-     def __trainsplit__(self):
-        return self.trainsplit
+
+        self.split = split # This defines the split you are going to use
+                           # (split files are called 'train.txt' and 'test.txt')
+
+        images_paths = []
+        labels = []
+
+        self.categories = os.listdir(self.root)
+        self.categories.remove("BACKGROUND_Google")
+
+        for path in open(f"./Caltech101/{self.split}.txt"):
+          path = path.replace("\n", "")
+          category = path.split("/")[0]
+          if(category != "BACKGROUND_Google"):
+            image_path = self.root + "/" + path
+            images_paths.append(image_path)
+            labels.append(self.categories.index(category))
+
+        self.data = pd.DataFrame(zip(images_paths, labels), columns = ["image_path", "label"])
         
-       
-     def __validsplit__(self):
-        return self.validsplit
-        
-     def __getitem__(self, index):
+
+    def __getitem__(self, index):
         '''
         __getitem__ should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
-        label= self.labels[index]
-        image = self.images[index] # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+        image_path = self.data["image_path"].iloc[index]
+        image, label = pil_loader(image_path), self.data["label"].iloc[index]
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -108,10 +63,10 @@ class Caltech(VisionDataset):
 
         return image, label
 
-     def __len__(self):
+    def __len__(self):
         '''
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = len(self.images) # Provide a way to get the length (number of elements) of the dataset
+        length = len(self.data) # Provide a way to get the length (number of elements) of the dataset
         return length
